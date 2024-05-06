9916acae2ab2159c9e884db0032ccbe75ddb8d9e
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-05-06 22:06:09 +0530

    adding ti github

---
+++ frontend/src/components/Receiver.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from "react";
+
+export function Receiver() {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        const socket = new WebSocket('ws://localhost:8080');
+        socket.onopen = () => {
+            socket.send(JSON.stringify({type: "receiver"}));
+        }
+
+        socket.onmessage = async (event) =>{
+            const message = JSON.parse(event.data);
+            let pc : RTCPeerConnection | null = null;
+            if(message.type === "createOffer"){
+                const pc = new RTCPeerConnection();
+                pc.setRemoteDescription(message.sdp);
+                pc.onicecandidate = (event) => {
+                    console.log(event);
+                    if(event.candidate){
+                        socket.send(JSON.stringify({type: 'iceCandidate', candidate: event.candidate}));
+                    }
+                }
+
+                pc.ontrack = (event) => {
+                    console.log(event);
+                    if (videoRef.current){
+                        videoRef.current.srcObject = new MediaStream([event.track]);
+                    }
+                }
+
+                const answer = await pc.createAnswer();
+                await pc.setLocalDescription(answer);
+                socket.send(JSON.stringify({type: "createAnswer", sdp: pc.localDescription}));
+            }else if (message.type === "iceCandidate"){
+                // @ts-ignore
+                pc.addIceCandidate(message.candidate);
+            }
+        }
+
+    }, []);
+
+    return <div>
+        receiver
+        <video ref={videoRef}></video>
+    </div>
+}
